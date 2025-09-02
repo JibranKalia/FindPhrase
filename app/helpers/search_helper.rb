ddff7874fe3b51f06a784d605b534a33b03c655f
Jibran Kalia <jibran.kalia@hey.com>
2025-09-02 12:15:06 -0500

    Better timestamp formatting

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -19,8 +19,23 @@ module SearchHelper
 
   def format_timestamp(timestamp)
     return "" if timestamp.blank?
-    # Format timestamp nicely (assumes format like "00:01:23")
-    timestamp
+    
+    # Convert from "00:18:16,000" format to "18:16"
+    if timestamp.match(/(\d{2}):(\d{2}):(\d{2}),?(\d{3})?/)
+      hours = $1.to_i
+      minutes = $2.to_i
+      seconds = $3.to_i
+      
+      # For episodes under 30 minutes, show as MM:SS
+      if hours == 0
+        "#{minutes}:#{seconds.to_s.rjust(2, '0')}"
+      else
+        # For longer episodes, show full HH:MM:SS
+        "#{hours}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+      end
+    else
+      timestamp
+    end
   end
 
   def search_suggestions
