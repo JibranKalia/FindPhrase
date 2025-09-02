7f72d746d4bf9850569d9306920def78788f1f43
Jibran Kalia <jibran.kalia@hey.com>
2025-09-02 12:04:15 -0500

    Add search as you type

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["form"]
+  static values = { debounce: { type: Number, default: 300 } }
+
+  connect() {
+    this.timeout = null
+  }
+
+  disconnect() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  search() {
+    // Clear existing timeout
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+
+    // Debounce the search
+    this.timeout = setTimeout(() => {
+      this.formTarget.requestSubmit()
+    }, this.debounceValue)
+  }
+}
